218ad56c8b0154623833a8c3e32f833d17f2806b
Sulyk <sylvain.boudacher@gmail.com>
2022-12-15 12:11:17 +0100

    Add NextUI + Navbar + pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import CustomNavbar from "./components/Navbar/navbar";
+import Login from "./routes/Login/login";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <BrowserRouter>
+      <NextUIProvider>
+        <CustomNavbar />
+        <Routes>
+          <Route path="/communication" element={<h1>Communication</h1>} />
+          <Route path="/salon" element={<h1>Salon</h1>} />
+          <Route path="/chat" element={<h1>Chat</h1>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<h1>Signup</h1>} />
+        </Routes>
+      </NextUIProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Navbar/navbar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  Navbar,
+  Button,
+  Image,
+  Link,
+  Text,
+  Card,
+  Spacer,
+  Radio,
+  useTheme,
+} from "@nextui-org/react";
+
+function CustomNavbar() {
+  return (
+    <Navbar>
+      <Navbar.Brand>
+        <Image
+          width={50}
+          src="https://play-lh.googleusercontent.com/CcTpODlVjkZJLgvRAoeTamFcJCYsjFtwgNK_SWGykfTzM0fLKPDSszws1Ga3kmxjhA"
+          alt="logo"
+        />
+      </Navbar.Brand>
+      {/* only if user is logged */}
+      <Navbar.Content>
+        <Navbar.Link>
+          <NavLink to="/communication">Communication</NavLink>
+        </Navbar.Link>
+        <Navbar.Link href="#">
+          <NavLink to="/salon">Salon</NavLink>
+        </Navbar.Link>
+        <Navbar.Link href="#">
+          <NavLink to="/chat">chat</NavLink>
+        </Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Content>
+        <Navbar.Link href="#">
+          <NavLink to="/login">Login</NavLink>
+        </Navbar.Link>
+        <Navbar.Item>
+          <Button auto flat as="a" href="#">
+            <NavLink to="/signup">Signup</NavLink>
+          </Button>
+        </Navbar.Item>
+      </Navbar.Content>
+    </Navbar>
+  );
+}
+
+export default CustomNavbar;
