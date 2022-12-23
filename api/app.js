c84e5cc986659350cde20714d090799d7cf2ceea
SylvainB-hollo <sylvain@hollo.io>
2022-12-23 17:56:05 +0100

    try to emmit message with socket.io

--- api/app.js
+++ api/app.js
@@ -7,6 +7,37 @@ dotenv.config();
 const app = express();
 const port = process.env.API_PORT || 3000;
 
+// Socket.io
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  // Send a message to client
+  socket.emit("message", "Hello from server");
+
+  // Listen to message from client
+  socket.on("clientMessage", (data) => {
+    console.log(data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server listening on port ${port} , , http://localhost:${port}`);
+});
+
 // Import routes
 import SecurityRouter from "./routes/Security.js";
 import SalonRouter from "./routes/Salon.js";
@@ -27,6 +58,6 @@ app.get("/test", (req, res) => {
 app.use(SecurityRouter);
 app.use(SalonRouter);
 
-app.listen(port, () =>
-  console.log(`Server running on port ${port}, http://localhost:${port}`)
-);
+// app.listen(port, () =>
+//   console.log(`Server running on port ${port}, http://localhost:${port}`)
+// );
---
+++ client/src/routes/Login/login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button, Navbar } from "@nextui-org/react";
+import CustomNavbar from "../../components/Navbar/navbar";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+export default Login;
