e9466aaa76461ae8e47e5d716656f0faa876fb4e
Norudah <pierucci.romain@gmail.com>
2022-12-26 18:35:38 +0100

    pouet

--- api/app.js
+++ api/app.js
@@ -1,25 +1,22 @@
-import express from "express";
-import * as dotenv from "dotenv";
 import cors from "cors";
-
-// Create Express server
-dotenv.config();
-const app = express();
-const port = process.env.API_PORT || 3000;
-
+import * as dotenv from "dotenv";
+import express from "express";
 // Socket.io
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
+// Import routes
+import SalonRouter from "./routes/Salon.js";
+import SecurityRouter from "./routes/Security.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.API_PORT || 3000;
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: [
-      "https://admin.socket.io",
-      "http://localhost:5173",
-      "http://localhost:3000",
-    ],
+    origin: ["https://admin.socket.io", "http://localhost:5173", "http://localhost:3000"],
     credentials: true,
   },
 });
@@ -53,9 +50,7 @@ io.on("connection", (socket) => {
     // add unique id to message
     let date = Date.now();
     let messageId = date + socket.id;
-    socket
-      .to(room)
-      .emit("message", message, room, socket.id, messageId, date, userName);
+    socket.to(room).emit("message", message, room, socket.id, messageId, date, userName);
     console.log("SocketIO: send-message", userName);
   });
 
@@ -64,13 +59,29 @@ io.on("connection", (socket) => {
   });
 });
 
+io.of("/admin").on("connection", (socket) => {
+  console.log("A admin has connected");
+
+  // Send a message to client
+  socket.emit("message", "Hello from server Admin!");
+
+  // Listen to message from client
+  socket.on("adminMessage", (message) => {
+    console.log(message);
+  });
+
+  socket.on("message", (message) => {
+    console.log(`Received message from admin: ${message}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("admin disconnected");
+  });
+});
+
 httpServer.listen(port, () => {
   console.log(`Server listening on port ${port} , , http://localhost:${port}`);
 });
-
-// Import routes
-import SecurityRouter from "./routes/Security.js";
-import SalonRouter from "./routes/Salon.js";
 
 app.use(express.json());
 app.use(cors({ credentials: true, origin: "*" }));
