efc95559ae0852eeb4f115e3099dfc16c9ddd344
Sulyk <sylvain.boudacher@gmail.com>
2023-01-30 22:08:55 +0100

    delet ReactQuery dev tools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,13 +3,12 @@ import App from "./App";
 import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={new QueryClient()}>
-    <App />
-    <ReactQueryDevtools initialIsOpen={true} />
+    <App />    
   </QueryClientProvider>
 );
